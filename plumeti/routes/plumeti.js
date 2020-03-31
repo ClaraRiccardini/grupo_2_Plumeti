af87fb78528893a21792e1f0df1225d20b5b15ab
ClaraRiccardini <61276778+ClaraRiccardini@users.noreply.github.com>
2020-03-31 12:01:17 -0300

    Avance del view de products y cart

--- plumeti/routes/plumeti.js
+++ plumeti/routes/plumeti.js
@@ -10,4 +10,12 @@ router.get('/home', function(req, res, next) {
     res.render('home', { title: 'Express' });
 });
 
+router.get('/products', function(req, res, next) {
+    res.render('products', { title: 'Express' });
+});
+
+router.get('/cart', function(req, res, next) {
+    res.render('cart', { title: 'Express' });
+});
+
 module.exports = router;
